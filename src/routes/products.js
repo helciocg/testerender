97475573f071f34b4a23d85dcb0fa7164a723d72
Helcio Caixeta <helcio.caixeta@gmail.com>
2023-09-12 00:07:55 -0300

    Correção de erro egine

--- src/routes/products.js
+++ src/routes/products.js
@@ -2,13 +2,16 @@ const { Router } = require('express');
 const app = Router();
   
 app.get('/products', (req, res) => {
-    //res.send("Produtos do banco de dados");
-
+    
+    
     require('dotenv').config()
+    
     const mysql = require('mysql2')
+    //res.send("Produtos do banco de dados...");
+    console.log(process.env.DATABASE_URL)
     const connection = mysql.createConnection(process.env.DATABASE_URL)
     console.log('Connected to PlanetScale!')
-
+    
     connection.query('SELECT * FROM products', function(err, rows, fields) {
         //rows.forEach(function(row) {
         //  console.log(row.id + '=> '+ row.name + ', '+ row.price);
@@ -20,10 +23,7 @@ app.get('/products', (req, res) => {
       console.log('Finish');
       });
     connection.end();
+    
 });
   
 module.exports = app;
-  
-    /*
-    
-    */
